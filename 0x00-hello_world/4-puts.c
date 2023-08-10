a7b5d5aaf15cb3889d9c93ca731f1176e6490653
hanaaattya <hanaaattya6565@gmail.com>
2023-08-10 12:46:19 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main(void){
-	puts("/"Programming is like building a multilingual puzzle");
+/**
+ *Main: entry  point
+ *
+ *Description: first c project
+ *
+ *Return =0 successful
+ */
 
-		return 0;
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return 0;
 }
