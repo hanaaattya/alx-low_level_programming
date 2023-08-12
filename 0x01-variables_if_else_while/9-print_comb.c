2e864406beb462fcd8d825d87c7d67cd488cc5c6
hanaaattya <hanaaattya6565@gmail.com>
2023-08-11 19:32:24 -0700

    if

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,14 @@
  */
 int main(void)
 {
-	int a,b;
+	int a;
 
-	for (a = '0'; a <= '9'; a++)
-	{
-	for (b = '0'; b <= '9'; b++)
+	for (a = '0'; a < '10'; a++)
 	{
 	putchar(a);
-	putchar(b);
-	if (a != 9 || b != 9)
+	if (a < 9)
 	{
-	putchar(',');
-	}
+	putchar(', ');
 	}
 	}
 	putchar('\n');
