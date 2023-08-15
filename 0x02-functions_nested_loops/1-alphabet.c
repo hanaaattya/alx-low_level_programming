023fc232c2585f1d00c14f00de10e558795e5909
hanaaattya <hanaaattya6565@gmail.com>
2023-08-15 14:01:38 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+}
