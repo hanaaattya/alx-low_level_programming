cad2916143a1aafa6ce060e343bdf7560001478b
hanaaattya <hanaaattya6565@gmail.com>
2023-08-15 14:24:50 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- print lowercase alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+	for (y = 'a' ; y <= 'z' ; y++)
+		_putchar(y);
+	}
+	_putchar('\n');
+}
