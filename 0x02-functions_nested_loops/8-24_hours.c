bb31d094487c17677d36f8f243e3e488c2cf7d56
hanaaattya <hanaaattya6565@gmail.com>
2023-08-15 18:51:03 -0700

    m

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,11 +11,13 @@ void jack_bauer(void)
 	for (hr = 0 ; hr < 24 ; hr++)
 {
 	for (min = 0 ; min < 60 ; min++)
+{
 	_putchar('0' + hr / 10);
 	_putchar('0' + hr % 10);
 	_putchar(':');
 	_putchar('0' + min / 10);
 	_putchar('0' + min % 10);
 	_putchar('\n');
+}
 }
 }
