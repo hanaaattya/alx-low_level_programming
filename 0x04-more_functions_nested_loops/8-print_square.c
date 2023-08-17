777231f77706d350a786e300db31808652d44172
hanaaattya <hanaaattya6565@gmail.com>
2023-08-17 10:20:51 -0700

    finish

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
+/**
+ * print_square - draws a square
+ *
+ * @size: square size
+ */
 void print_square(int size)
 {
+	int i, j;
 
+	if (size <= 0)
+	_putchar('\n');
+	else
+	{
+	for (i = 1 ; i <= size ; i++)
+	{
+		for (j = 1 ; j <= size ; j++)
+			_putchar('#');
+	_putchar('\n');
+	}
+	}
 }
