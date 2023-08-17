5e406b2470707f4b5192eed2d22edbec75cc837e
hanaaattya <hanaaattya6565@gmail.com>
2023-08-17 15:30:33 -0700

    .

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,6 @@
 
 /**
  * print_triangle - draws triangle
- * Description: triangle draws
  * @size: triangke size
  */
 void print_triangle(int size)
@@ -10,16 +9,21 @@ void print_triangle(int size)
 	int i, j;
 
 	if (size <= 0)
+	
 	_putchar('\n');
-	else
+	
+else
 {
 	for (i = 1; i <= size; i++)
 {
 	for (j = 1; j <= size; j++)
 {
 	if ((i + j) < size)
+	
 	_putchar(' ');
+	
 	else
+	
 	_putchar('#');
 }
 	_putchar('\n');
