32ca2a96555a355cb41669ebc037742aa59faf8e
hanaaattya <hanaaattya6565@gmail.com>
2023-08-17 12:42:19 -0700

    finish

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,10 @@ void print_triangle(int size)
 {
 	int i, j;
 
+	if (size <=0)
+	_putchar('\n');
+	else
+{
 	for (i = 1; i <= size; i++)
 {
 	for (j = 1; j <= size; j++)
@@ -19,5 +23,6 @@ void print_triangle(int size)
 	_putchar('#');
 }
 	_putchar('\n');
+}
 }
 }
