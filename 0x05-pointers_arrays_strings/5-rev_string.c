c6cd187884a14879749c3da9c4d7605b8567c201
hanaaattya <hanaaattya6565@gmail.com>
2023-08-21 19:09:12 -0700

    gi

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * rev_string - prints a reverse string
+ * @s: int parameter
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+	length++
+	}
+	for (int i = 0; i < length / 2; i++)
+	{
+	char temp = s[i];
+	s[i] = s[length - i - 1];
+	s[length - i - 1] = temp;
+	}
+}
