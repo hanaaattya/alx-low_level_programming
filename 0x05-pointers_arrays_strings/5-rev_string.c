c04b9dfa95d4dd6c4298b8418911bdfd2f2b19c5
hanaaattya <hanaaattya6565@gmail.com>
2023-08-21 19:10:55 -0700

    gi

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,9 +15,9 @@ void rev_string(char *s)
 
 	while (s[length] != '\0')
 	{
-	length++
+	length++;
 	}
-	for (int i = 0; i < length / 2; i++)
+	for (i = 0; i < length / 2; i++)
 	{
 	char temp = s[i];
 	s[i] = s[length - i - 1];
