5d91cf85dcbfd682d8d8852e6812b461eb1871a7
root <root@DESKTOP-BVL45MJ.localdomain>
2023-09-02 05:22:36 +0300

    argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,14 @@
  * main -  prints the number of arguments passed into it.
  *
  *@argc: var
+ *@argv: ptr
  *
  * Return: Always 0.
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
+	(void) argv;
+
 	printf("%d\n", argc - 1);
 	return (0);
 }
