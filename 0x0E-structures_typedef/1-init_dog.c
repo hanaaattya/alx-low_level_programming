7127939a23dceaf31634f1d4eacc4db6650ecfd5
hanaaattya <hanaaattya6565@gmail.com>
2023-09-11 18:07:38 +0300

    struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,6 +6,8 @@
  * @name: Pointer to  dog's name string
  * @age: dog's age
  * @owner: Pointer to the owner's name
+ *
+ * return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
