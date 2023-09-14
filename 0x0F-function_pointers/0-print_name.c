9b75bef56998b2ea59357876260d7b48e89db28c
hanaaattya <hanaaattya6565@gmail.com>
2023-09-14 13:02:58 +0300

    m

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,13 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name
+ * @name: name
+ * @f: ptr
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name && f)
 	{
 	f(name);
 	}
