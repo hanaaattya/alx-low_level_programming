8114f8bd1684bceea07732e0392c29e07040ca3a
hanaaattya <hanaaattya6565@gmail.com>
2023-09-15 05:56:07 +0300

    variadic

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+void print_all(const char * const format, ...);
+{
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,13 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+}
+token_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
