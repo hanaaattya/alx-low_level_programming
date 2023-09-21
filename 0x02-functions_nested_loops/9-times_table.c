246e2d6805ce99c8c9867f3eaab62fea40fdfb21
hanaaattya <hanaaattya6565@gmail.com>
2023-09-21 21:52:21 +0300

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,9 @@ void times_table(void)
 {
 	int i, prod, j;
 
-	for (i = 0 ; i < 9 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 {
-	for (j = 0; j < 9 ; j++)
+	for (j = 0; j <= 9 ; j++)
 	{
 	prod = i * j;
 	if (prod < 10)
