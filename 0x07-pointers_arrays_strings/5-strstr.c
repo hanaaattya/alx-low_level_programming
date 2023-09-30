d2ed82e7a49630839354e76fa84f4d93a3073743
hanaaattya <hanaaattya6565@gmail.com>
2023-09-30 16:50:10 +0300

    m

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,14 +1,32 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * *_strstr - locates a substring.
  *
- *@dhaystack: ptr
- *@needle: var
+ *@haystack: ptr
+ *@needle: ptr
  *
- * Return: Always 0.
+ * Return: haystack if found or NULL
  */
 char *_strstr(char *haystack, char *needle)
 {
-	return (0);
+	char *needle_cpy = needle;
+	char *haystack_cpy = haystack;
+
+	if (*needle_cpy == '\0')
+		return (haystack);
+
+	while (*haystack_cpy != '\0'){
+	while (*needle_cpy !='\0' && *haystack_cpy == *needle_cpy)
+	{
+	needle_cpy++;
+	haystack_cpy++;
+	}
+	if (*needle_cpy == '\0')
+		return (haystack);
+
+	haystack_cpy++;
+}
+	return (NULL);
 }
