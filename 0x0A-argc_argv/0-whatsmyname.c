f18246467a0652435de8ec6962e290e55671558b
hanaaattya <hanaaattya6565@gmail.com>
2023-10-03 01:29:27 +0300

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,12 @@
  * @argv: ptr
  * @argc: int
  *
- * return: 0
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
+
 	printf("%s\n", argv[0]);
-		return (0);
+	return (0);
 }
