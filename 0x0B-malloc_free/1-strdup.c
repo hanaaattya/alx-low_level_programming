aa9f554dc29d6e5e683b07532278018ec052e990
hanaaattya <hanaaattya6565@gmail.com>
2023-10-04 20:52:10 +0300

    m

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicate string
+ * @str: string
+ *
+ * Return: duplicated_str.
+ */
+char *_strdup(char *str)
+{
+	char *duplicated_str;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	duplicated_str = (char *)malloc(strlen(str + 1) * sizeof(char));
+	if (duplicated_str == NULL)
+	{
+	return (NULL);
+	}
+	strcpy(duplicated_str, str);
+	return (duplicated_str);
+}
