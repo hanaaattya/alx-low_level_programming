54a070a8bac34bfac92b27309f1f35df9298f074
hanaaattya <hanaaattya6565@gmail.com>
2023-10-11 05:15:33 +0300

    m

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initializes structure
+ * @d: Ptr to the dog structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if(d)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
