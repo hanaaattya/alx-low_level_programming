d6539a119c6f13eea981eddb85f2b707f4011b37
hanaaattya <hanaaattya6565@gmail.com>
2023-10-13 05:56:13 +0300

    m

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,9 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_int(va_list argsp);
+void print_char(va_list argsp);
+void print_float(va_list argsp);
+void print_string(va_list argsp);
 void print_all(const char * const format, ...);
 #endif
