2e61fbed0ba667485acfdae5e04af00b3f960593
hanaaattya <hanaaattya6565@gmail.com>
2023-10-23 19:50:02 +0300

    m

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,11 @@
+#include <stddef.h>
+#ifndef LISTS_H
+#define LISTS_H
+typedef struct list_s
+{
+    char *str;
+    int len;
+    struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+#endif
