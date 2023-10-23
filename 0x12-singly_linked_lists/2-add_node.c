405b5f50d81dad6726640f91c1029505eeb36f56
hanaaattya <hanaaattya6565@gmail.com>
2023-10-23 21:19:34 +0300

    m

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * *add_node - add new node
+ * @head: ptr
+ * @str: ptr
+ *
+ * Return: new
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (str == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+	free(new);
+	return (NULL);
+	}
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
