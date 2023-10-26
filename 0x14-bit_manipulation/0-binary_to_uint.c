eb8853d0f37c874994e917b31e364b92f2e858b6
hanaaattya <hanaaattya6565@gmail.com>
2023-10-27 01:31:33 +0300

    m

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * binary_to_uint - Converts a binary str to regular number unsigned int
+ * @b: ptr to the binary str
+ * Return: The converted number, or 0 if error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int converted = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+		for (i = 0; b[i] != '\0'; i++)
+		{
+			if (b[i] == '0')
+				converted = converted * 2;
+			else if (b[i] == '1')
+	       			converted = converted * 2 + 1;
+			else
+				return (0);
+		}
+		return (converted);
+}
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - Sets the value of any  bit to 1 at a given index
+ * @n: ptr to num to set the bit
+ * @index: The index of the bit to set
+ * Return: 1 if worked or -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	 unsigned long int i;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+	return (-1);
+	}
+	i = 1UL << index;
+	*n = *n | i;
+	return (1);
+}
