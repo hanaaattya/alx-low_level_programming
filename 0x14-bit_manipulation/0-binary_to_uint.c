673075505c5a6ac7b7f46f7c04e33214c60c1e7d
hanaaattya <hanaaattya6565@gmail.com>
2023-10-27 02:18:13 +0300

    m

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,16 +13,16 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 	{
-		return (0);
+	return (0);
 	}
-		for (i = 0; b[i] != '\0'; i++)
-		{
-			if (b[i] == '0')
-				converted = converted * 2;
-			else if (b[i] == '1')
-	       			converted = converted * 2 + 1;
-			else
-				return (0);
-		}
-		return (converted);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+	if (b[i] == '0')
+	converted = converted * 2;
+	else if (b[i] == '1')
+	converted = converted * 2 + 1;
+	else
+	return (0);
+	}
+	return (converted);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - Counts the num of bits needed to flip to transform one binary num into another
+ * flip_bits - Counts the num of bits
  * @n: int
  * @m: int
  * Return: num of bits to flip
@@ -11,11 +11,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int xor = n ^ m;
 	unsigned int counter = 0;
 
-	 while (xor > 0)
-	 {
-	 counter += xor & 1;
-	 xor >>= 1;
-	 }
-	 return (counter);
+	while (xor > 0)
+	{
+	counter += xor & 1;
+	xor >>= 1;
+	}
+	return (counter);
 }
 
