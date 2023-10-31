359d27163652a2d460642956ba4b501d9b651fc9
hanaaattya <hanaaattya6565@gmail.com>
2023-10-31 10:47:16 +0300

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,23 +1,25 @@
 #include "main.h"
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 
+/**
+ * read_textfile - Reads and prints the content of a file
+ * to the POSIX std output
+ * @filename: Name of the file to read
+ * @letters: Num of letters to read and print
+ * Return: num of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int f_descriptor;
 	int read_letters;
 	int write_letters;
 	char *buffer;
+
 	if (filename == NULL)
 		return (0);
 	f_descriptor = open(filename, O_RDONLY);
 	if (f_descriptor == -1)
 		return (0);
-	buffer = malloc(sizeof (char) * letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(f_descriptor);
