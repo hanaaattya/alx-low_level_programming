a564aa2226841ca3680ce97b4e99b1ede51d044f
hanaaattya <hanaaattya6565@gmail.com>
2023-11-03 20:41:20 +0300

    m

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of the file
+ * @text_content: the content
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len;
+	int write_text;
+
+	if (filename == NULL)
+	{
+	return (-1);
+	}
+	if (text_content == NULL)
+	{
+	text_content = "";
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+	return (-1);
+	}
+	for (len = 0; text_content[len] != '\0'; len++)
+	write_text = write(fd, text_content, len);
+	close(fd);
+	if (write_text == -1)
+	{
+	return (-1);
+	}
+	return (1);
+}
